899acca242b6eacf6d60cff965c0a866c84c2660
casey <caseymwangi31@gmail.com>
2024-06-13 15:28:06 +0300

    understand dsa

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,16 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    return  self._bfs(self.root, id)
+  
+  def _bfs(self, root, id):
+    if root is None:
+      return None
+    queue = [root]
+    while queue:
+      node = queue.pop(0)
+      if node.get('id') == id:
+        return node
+      queue.extend(node.get('children', []))
+    return None
+     
